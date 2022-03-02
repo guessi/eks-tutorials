5d05c052609d47a57fc7aebcdf982931d86bfc96
guessi <guessi@gmail.com>
2022-03-03 00:32:55 +0800

    Update script to support different version of AWS Load Balancer Controller

--- scripts/install-aws-load-balancer-controller.sh
+++ scripts/install-aws-load-balancer-controller.sh
@@ -3,6 +3,21 @@
 AWS_REGION="us-east-1"
 EKS_CLUSTER_NAME="eks-demo"
 
+# CHART VERSION	APP VERSION
+# ---------------------------
+# 1.2.2        	v2.2.0
+# 1.2.3        	v2.2.1
+# 1.2.5        	v2.2.2
+# 1.2.6        	v2.2.3
+# 1.2.7        	v2.2.4
+# 1.3.1        	v2.3.0
+# 1.3.2        	v2.3.0
+# 1.3.3        	v2.3.1
+# 1.4.0        	v2.4.0
+
+APP_VERSION="v2.4.0"
+CHART_VERSION="1.4.0"
+
 echo "[debug] detecting AWS Account ID"
 export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 echo "[debug] AWS Account ID: ${AWS_ACCOUNT_ID}"
@@ -30,7 +45,7 @@ aws iam list-policies --query "Policies[].[PolicyName,UpdateDate]" --output text
 if [ $? -ne 0 ]; then
   echo "[debug] IAM policy 'AWSLoadBalancerControllerIAMPolicy' existance not found, creating"
   curl -o aws-load-balancer-controller-policy.json \
-    https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.4.0/docs/install/iam_policy.json
+    https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/${APP_VERSION}/docs/install/iam_policy.json
   aws iam create-policy \
     --policy-name AWSLoadBalancerControllerIAMPolicy \
     --policy-document file://aws-load-balancer-controller-policy.json
@@ -56,6 +71,7 @@ if [ $? -ne 0 ]; then
   helm upgrade \
     --namespace kube-system \
     --install aws-load-balancer-controller \
+    --version ${CHART_VERSION} \
     eks/aws-load-balancer-controller \
       --set clusterName=${EKS_CLUSTER_NAME} \
       --set serviceAccount.create=false \
