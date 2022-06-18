c2d2189347c108451ef31fe0c43cf82eaafa679c
guessi <guessi@gmail.com>
2022-06-18 14:03:06 +0800

    Update support for Karpenter 0.11.1

--- scripts/karpenter/setup.sh
+++ scripts/karpenter/setup.sh
@@ -8,11 +8,12 @@ ROLE_NAME="${EKS_CLUSTER_NAME}-karpenter"
 
 # CHART VERSION	APP VERSION
 # ---------------------------
+# 0.11.1        	0.11.1
 # 0.11.0        	0.11.0
 # 0.10.1        	0.10.1
 
-APP_VERSION="0.11.0"
-CHART_VERSION="0.11.0"
+APP_VERSION="0.11.1"
+CHART_VERSION="0.11.1"
 
 echo "[debug] detecting AWS Account ID"
 export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
